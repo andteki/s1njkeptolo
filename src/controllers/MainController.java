5a261e431f0e0e66e73800ce9d4c4c17b5f8fc82
Sallai András <andrewteki@gmail.com>
2022-05-17 10:50:41 +0200

    Jobbra irány vizsgálat működik

--- src/controllers/MainController.java
+++ src/controllers/MainController.java
@@ -10,6 +10,9 @@ import views.MainFrame;
 public class MainController {
     MainFrame mainFrame;
     MainModel mainModel;
+    enum Irany {
+        ESZAK, DEL, KELET, NYUGAT, NO
+    }
     public MainController() {
         this.mainFrame = new MainFrame();
         this.mainModel = new MainModel();
@@ -24,9 +27,29 @@ public class MainController {
     private void onClickButtons(ActionEvent event) {
         
         JButton button = (JButton) event.getSource();
+
+        checkDirection(button);
+    }
+    private void checkDirection(JButton button) {
+        //jobbra?
         int x = button.getLocation().x;
         int y = button.getLocation().y;
         System.out.println(x + " " + y);
-
+        Irany irany = Irany.NO;
+        if(x<750 && checkRight(x, y)) {
+            irany = Irany.KELET;
+        }
+        System.out.println(irany);
+    }
+    private boolean checkRight(int act_x, int act_y) {
+        boolean direction_ok = true;
+        for(int i=0; i<15; i++) {
+            int x = mainFrame.buttons[i].getLocation().x;
+            int y = mainFrame.buttons[i].getLocation().y;
+            if(act_y == y && act_x + 250 == x) {
+                direction_ok = false;
+            }
+        }
+        return direction_ok;
     }
 }
